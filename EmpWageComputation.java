28cb1787d281d012168b41a9d3a7bb7997cf6b0d
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-31 21:02:52 +0530

    Welcome To Emp Wage Computatin in Java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,6 +1,1 @@
-public class EmpWageComputation {
-	
-     public static void main(String[] args) {
-    	 System.out.println("Employee Wage Computation Program in Java");
-     }
-}
+//Employee Wage Computation Program in Java
