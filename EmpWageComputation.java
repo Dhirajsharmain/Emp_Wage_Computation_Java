c12a19f1e5965813f8c9cb696896623dc9bf3b0e
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-01 09:09:27 +0530

    Added: Part Time Wage Calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,16 +5,21 @@ public class EmpWageComputation {
     	 //constants
     	 int present = 1;
     	 int empRatePerHour = 20;
+    	 int isFullTime = 1;
+    	 int isPartTime = 2;
     	 
     	 //variables
          int empWage = 0;
          int empHrs = 0;
               
-         double empCheck = Math.floor(Math.random() * 10) % 2;
+         double empCheck = Math.floor(Math.random() * 10) % 3;
               
-         if (empCheck == present) {
+         if (empCheck == isFullTime) {
         	 System.out.println("Employee is Present");
         	 empHrs = 8;
+         }
+         else if(empCheck == isPartTime) {
+        	 empHrs = 4;
          }
          else {  
         	 System.out.println("Employee is Absent");
