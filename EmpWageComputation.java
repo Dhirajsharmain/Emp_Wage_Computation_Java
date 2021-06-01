ca419203bfe48063ad9b5fb00104464320ff7438
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-01 08:39:27 +0530

    Added: Calculating Daily Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,13 +2,25 @@
 
 public class EmpWageComputation {
      public static void main(String[] args) {
-
-              int present = 1;
-              double empCheck = Math.floor(Math.random() * 10) % 2;
+    	 //constants
+    	 int present = 1;
+    	 int empRatePerHour = 20;
+    	 //variables
+         int empWage = 0;
+         int empHrs = 0;
               
-              if (empCheck == present)
-                    System.out.println("Employee is Present");
-              else
-                    System.out.println("Employee is Absent");
+         double empCheck = Math.floor(Math.random() * 10) % 2;
+              
+         if (empCheck == present) {
+        	 System.out.println("Employee is Present");
+        	 empHrs = 8;
+         }
+         else {  
+            System.out.println("Employee is Absent");
+             empHrs = 0;
+         }
+              
+          empWage = empHrs * empRatePerHour;
+          System.out.println("EmpWage: " +empWage);
         }
 }
